4a716c468c3bc85c2046fc41799e15e59b3d23f3
subhashrsrivatsa <subhashrsrivatsa.paris@atlassian.com>
2021-03-07 22:59:09 +0100

    initial commit

---
+++ LeetCode/src/leetCode/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package leetCode;
+
+/**
+ * @author Subhash
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+
+	}
+
+}
